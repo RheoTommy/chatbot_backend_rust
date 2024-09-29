6fa67d874f5f78681e15465d6a18166635b6fb2d
rheotommy <tommyrheo@gmail.com>
2024-09-29 22:25:34 +0900

    refactor: use named type for each id

--- src/domain/entities/conversation.rs
+++ src/domain/entities/conversation.rs
@@ -1,9 +1,14 @@
+use crate::domain::entities::user::UserId;
+use serde::{Deserialize, Serialize};
 use std::time::SystemTime;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ConversationId(uuid::Uuid);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Conversation {
-    pub id: uuid::Uuid,
-    pub user_id: uuid::Uuid,
+    pub id: ConversationId,
+    pub user_id: UserId,
     pub title: String,
     pub created_at: SystemTime,
     pub updated_at: SystemTime,
--- src/domain/entities/message.rs
+++ src/domain/entities/message.rs
@@ -1,13 +1,20 @@
+use crate::domain::entities::conversation::ConversationId;
+use serde::{Deserialize, Serialize};
 use std::time::SystemTime;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MessageId(uuid::Uuid);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum MessageFrom {
     User(uuid::Uuid),
     Bot,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Message {
-    pub id: uuid::Uuid,
-    pub conversation_id: uuid::Uuid,
+    pub id: MessageId,
+    pub conversation_id: ConversationId,
     pub from: MessageFrom,
     pub content: String,
     pub created_at: SystemTime,
